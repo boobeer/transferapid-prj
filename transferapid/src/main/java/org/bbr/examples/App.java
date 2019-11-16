d6374f93a5207a8ecd997579d58ce017f63ec9f6
boobeer <emmooe@gmail.com>
2019-11-16 16:29:12 +0100

    initial-project-structure

---
+++ transferapid/src/main/java/org/bbr/examples/App.java
@@ -0,0 +1,13 @@
+package org.bbr.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
